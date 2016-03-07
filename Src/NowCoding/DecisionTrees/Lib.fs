17a0998c357e37c6ac18f46a9e7bf8049188761c
polkduran <polkduran@hotmail.com>
2016-03-08 00:11:52 +0100

    Add DecisionTrees project

---
+++ Src/NowCoding/DecisionTrees/Lib.fs
@@ -0,0 +1,11 @@
+﻿module DecisionTrees
+open System
+
+let entropy (data:seq<'T>) (selector:'T -> 'U) =
+    let count = data |> Seq.length |> float
+    let entr = data 
+                    |> Seq.groupBy selector
+                    |> Seq.map (snd >> Seq.length >> float >> (/)count)
+                    |> Seq.sumBy (fun px -> px*Math.Log(px,2.0))
+    -entr
+
---
+++ Src/NowCoding/DecisionTrees/Script.fsx
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Lib.fs"
+open DecisionTrees
+
+let d = [(1.0,true);(2.0,true);(3.0,true);(2.0,true);(2.0,true)]
+
+let efst = entropy d fst
+let esnd = entropy d snd
