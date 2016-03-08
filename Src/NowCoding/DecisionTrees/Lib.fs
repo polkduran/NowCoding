6fbc241fad20fdcacdff33db3155f67d21570b49
Pablo Fernandez Duran <polkduran@users.noreply.github.com>
2016-03-08 10:58:48 +0100

    Update Lib.fs
    Fix entropy function

--- Src/NowCoding/DecisionTrees/Lib.fs
+++ Src/NowCoding/DecisionTrees/Lib.fs
@@ -5,7 +5,7 @@ let entropy (data:seq<'T>) (selector:'T -> 'U) =
     let count = data |> Seq.length |> float
     let entr = data 
                     |> Seq.groupBy selector
-                    |> Seq.map (snd >> Seq.length >> float >> (/)count)
+                    |> Seq.map (snd >> Seq.length >> float >> fun c -> c/count)
                     |> Seq.sumBy (fun px -> px*Math.Log(px,2.0))
     -entr
 
