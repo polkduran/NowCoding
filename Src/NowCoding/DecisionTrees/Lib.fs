46376f00c7f2c8951f08bb60101701befa94acc7
Pablo Fernandez Duran <polkduran@users.noreply.github.com>
2016-03-09 18:21:54 +0100

    Entropy gain function

--- Src/NowCoding/DecisionTrees/Lib.fs
+++ Src/NowCoding/DecisionTrees/Lib.fs
@@ -1,11 +1,31 @@
 ﻿module DecisionTrees
-open System
+﻿
+// https://en.wikipedia.org/wiki/ID3_algorithm
+// http://stackoverflow.com/questions/1418829/help-needed-creating-a-binary-tree-given-truth-table
+// https://blogs.msdn.microsoft.com/chrsmith/2009/10/31/awesome-f-decision-trees-part-i/
 
 let entropy (data:seq<'T>) (selector:'T -> 'U) =
     let count = data |> Seq.length |> float
-    let entr = data 
-                    |> Seq.groupBy selector
-                    |> Seq.map (snd >> Seq.length >> float >> fun c -> c/count)
-                    |> Seq.sumBy (fun px -> px*Math.Log(px,2.0))
-    -entr
+    let splitDataByFeature = data 
+                                |> Seq.groupBy selector // seq<'U * seq<'T>>
+                                |> Seq.map (fun (x, gx) -> x, gx |> Seq.toArray) //seq<'U * 'T[]>
+    
+    let partitionProb = splitDataByFeature |> Seq.map (fun (x, gx) -> x, float(gx.Length)/count ) //seq<'U * float>
+    let entr = partitionProb 
+                |> Seq.map snd // seq<float>
+                |> Seq.sumBy (fun px -> px * Math.Log(px, 2.0))
+                |> fun e -> -e
+                
+    entr, splitDataByFeature
+  
+let gain (data:seq<'T>) (selector:'T -> 'U) (selector2:('T -> 'V)) =
+    let count = data |> Seq.length |> float
+    let dataEntr, splitDataByFeature = entropy data selector
+    
+    let splitDataEntr = splitDataByFeature 
+                        |> Seq.map snd //seq<'T[]>
+                        |> Seq.map (fun part -> float(part.Length)/count, entropy part selector2  |> fst ) // seq<float * float>
+                        |> Seq.sumBy (fun (pt,ht) -> pt*ht)
+    
+    dataEntr-splitDataEntr
 
