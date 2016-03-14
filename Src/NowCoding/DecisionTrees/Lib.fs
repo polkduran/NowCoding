3efb8e7aaeb97d50ecda82822305a3c9d68eebac
polkduran <polkduran@hotmail.com>
2016-03-14 23:59:14 +0100

    display tree function

--- Src/NowCoding/DecisionTrees/Lib.fs
+++ Src/NowCoding/DecisionTrees/Lib.fs
@@ -1,109 +1,131 @@
-module DecisionTrees﻿
+namespace NowCoding
 
-// https://en.wikipedia.org/wiki/ID3_algorithm
+module DecisionTrees =
+
+    // https://en.wikipedia.org/wiki/ID3_algorithm
 // http://stackoverflow.com/questions/1418829/help-needed-creating-a-binary-tree-given-truth-table
 // https://blogs.msdn.microsoft.com/chrsmith/2009/10/31/awesome-f-decision-trees-part-i/
 // http://clear-lines.com/blog/post/Decision-Tree-classification.aspx
 
-let private entropyInternal (categorySelector:'T -> 'U) (data:'T seq) =
-    let log2 x = System.Math.Log(x, 2.0)
-    let dataCount = data |> Seq.length
-
-    let probByCategory = data 
-                            |> Seq.groupBy categorySelector // seq<'U * seq<'T>>
-                            |> Seq.map (fun (a, ga) -> ga |> Seq.length |> float) //seq<float>
-                            |> Seq.map (fun pa -> pa/float(dataCount) ) //seq<float>
-    let entr = probByCategory 
-                |> Seq.sumBy (fun px -> px * log2 px)
-                |> fun e -> -e
-                
-    entr, dataCount
-
-let entropy (categorySelector:'T -> 'U) (data:'T seq) = entropyInternal categorySelector data |> fst
-  
-let gain (data:'T seq) (categorySelector:'T -> 'U) (featureSelector:('T -> obj)) =
-    let dataEntr, dataCount = entropyInternal categorySelector data
-    let partDataEntr = data 
-                        |> Seq.groupBy featureSelector
-                        |> Seq.map (fun (a, part) -> part, part |> Seq.length |> float)
-                        |> Seq.map (fun (part, partCount) -> partCount/float(dataCount), entropy categorySelector part) // seq<float * float>
-                        |> Seq.sumBy (fun (pt, ht) -> pt * ht)
-    
-    dataEntr-partDataEntr
-
-type TreeNode<'T, 'U> = 
+    type TreeNode<'T, 'U> = 
         |Category of 'U * float
-        |Choice of ('T -> obj) * (obj * TreeNode<'T, 'U>) list
+        |Choice of string * ('T -> obj) * (obj * TreeNode<'T, 'U>) list
 
-type Tree<'T, 'U> = |Root of TreeNode<'T, 'U>
+    type Tree<'T, 'U> = |Root of TreeNode<'T, 'U>
 
-let classify (datum:'T) (tree:Tree<'T, 'U>) = 
-    let rec chooseNode (selector:('T -> obj)) (nodes:( obj * TreeNode<'T, 'U>) list) =
-        match nodes with
-        | (value, node)::_ when selector(datum) = value -> node
-        | c::r -> chooseNode selector r
-        | [] -> failwith <| sprintf "no node for selector %A and datum %A" selector datum
+    let private entropyInternal (categorySelector:'T -> 'U) (data:'T seq) =
+        let log2 x = System.Math.Log(x, 2.0)
+        let dataCount = data |> Seq.length
 
-    let rec classifyInt (node:TreeNode<'T, 'U>) =
-        match node with
-        | Category(category, prob) -> category
-        | Choice(selector, nodes) -> chooseNode selector nodes |> classifyInt
+        let probByCategory = data 
+                                |> Seq.groupBy categorySelector // seq<'U * seq<'T>>
+                                |> Seq.map (fun (a, ga) -> ga |> Seq.length |> float) //seq<float>
+                                |> Seq.map (fun pa -> pa/float(dataCount) ) //seq<float>
+        let entr = probByCategory 
+                    |> Seq.sumBy (fun px -> px * log2 px)
+                    |> fun e -> -e
+                
+        entr, dataCount
 
-    let root = match tree with |Root(r) -> r
-    let value = classifyInt root
-    value
-
-let trainID3 (data: 'T seq) (categorySelector:'T -> 'U) (features: ('T -> obj) list) = 
-    let rec getOptFeature (feature:(('T -> obj) * float) option) (restFeatures: ('T -> obj) list) restData acc =
-        match feature, restFeatures with
-        | None, [] -> failwith "max with empty"
-        | None, f::[] -> f, acc
-        | None, f::r -> getOptFeature (Some(f, gain restData categorySelector f))  r restData acc
-        | Some(f, gainF), [] -> f, acc
-        | Some(f1, gainF1), f2::r -> 
-                            let gainF2 = gain restData categorySelector f2
-                            if gainF1 > gainF2 then
-                                getOptFeature (Some(f1,gainF1)) r restData (f2::acc)
-                            else
-                                getOptFeature (Some(f2,gainF2)) r restData (f1::acc)
-
-    let getMaxCatProb featureData =
-        let len = Seq.length >> float
-        let dataCount = featureData |> len
-        let featDataByCatProb = featureData 
-                                |> Seq.groupBy categorySelector
-                                |> Seq.map (fun (cat, d) -> cat, (d |> len) / dataCount)
-        let maxCat = featDataByCatProb |> Seq.maxBy snd //|> fst
-        maxCat
-        
-    let rec trainID3Int (restData: 'T seq) (restFeatures: ('T -> obj) list) =
-        match restFeatures with
-        | [] -> failwith "emtpy features"
-        | featureSelector::[] ->  
-                let partData = restData 
+    let entropy (categorySelector:'T -> 'U) (data:'T seq) = entropyInternal categorySelector data |> fst
+  
+    let gain (data:'T seq) (categorySelector:'T -> 'U) (featureSelector:('T -> obj)) =
+        let dataEntr, dataCount = entropyInternal categorySelector data
+        let partDataEntr = data 
                             |> Seq.groupBy featureSelector
-                            |> Seq.map (fun (feature, featData) -> feature, getMaxCatProb featData) 
-                            |> Seq.map (fun (feature, cat) -> feature, Category(cat)) |> Seq.toList
-                Choice(featureSelector, partData)
-        | _ -> let bestFeature, others =  getOptFeature None restFeatures restData []
-               let uniqueCategory = Seq.groupBy categorySelector >> Seq.length >> (=)1
-               let catPart, choicePart = restData 
-                                            |> Seq.groupBy bestFeature |> Seq.toList
-                                            |> List.partition (snd >> uniqueCategory)
-               let catNodes = catPart 
-                                |> Seq.map (fun (featVal, featData) -> featVal, Category(featData |> Seq.head |> categorySelector, 1.0))
-                                |> Seq.toList
-               let choiceNodes = choicePart 
-                                    |> Seq.map (fun (featVal, featData) -> featVal, trainID3Int featData others )
+                            |> Seq.map (fun (a, part) -> part, part |> Seq.length |> float)
+                            |> Seq.map (fun (part, partCount) -> partCount/float(dataCount), entropy categorySelector part) // seq<float * float>
+                            |> Seq.sumBy (fun (pt, ht) -> pt * ht)
+    
+        dataEntr-partDataEntr
+
+    let classify (datum:'T) (tree:Tree<'T, 'U>) = 
+        let rec chooseNode feature (selector:('T -> obj)) (nodes:( obj * TreeNode<'T, 'U>) list) =
+            match nodes with
+            | (value, node)::_ when selector(datum) = value -> node
+            | c::r -> chooseNode feature selector r
+            | [] -> failwith <| sprintf "no node for feature %s and datum %A" feature datum
+
+        let rec classifyInt (node:TreeNode<'T, 'U>) =
+            match node with
+            | Category(category, prob) -> category
+            | Choice(feature, selector, nodes) -> chooseNode feature selector nodes |> classifyInt
+
+        let root = match tree with |Root(r) -> r
+        let value = classifyInt root
+        value
+
+    let trainID3 (data: 'T seq) (categorySelector:'T -> 'U) (features: (string * ('T -> obj)) list) = 
+        let rec getOptFeature (feature:(string * ('T -> obj) * float) option) (restFeatures: (string * ('T -> obj)) list) restData acc =
+            match feature, restFeatures with
+            | None, [] -> failwith "max with empty"
+            | None, (n,f)::[] -> n, f, acc
+            | None, (n,f)::r -> getOptFeature (Some(n, f, gain restData categorySelector f))  r restData acc
+            | Some(n, f, gainF), [] -> n, f, acc
+            | Some(n1, f1, gainF1), (n2,f2)::r -> 
+                                let gainF2 = gain restData categorySelector f2
+                                if gainF1 > gainF2 then
+                                    getOptFeature (Some(n1,f1,gainF1)) r restData ((n2,f2)::acc)
+                                else
+                                    getOptFeature (Some(n2,f2,gainF2)) r restData ((n1,f1)::acc)
+
+        let getMaxCatProb featureData =
+            let len = Seq.length >> float
+            let dataCount = featureData |> len
+            let featDataByCatProb = featureData 
+                                    |> Seq.groupBy categorySelector
+                                    |> Seq.map (fun (cat, d) -> cat, (d |> len) / dataCount)
+            let maxCat = featDataByCatProb |> Seq.maxBy snd //|> fst
+            maxCat
+        
+        let rec trainID3Int (restData: 'T seq) (restFeatures: (string * ('T -> obj)) list) =
+            match restFeatures with
+            | [] -> failwith "emtpy features"
+            | (feature, featureSelector)::[] ->  
+                    let partData = restData 
+                                |> Seq.groupBy featureSelector
+                                |> Seq.map (fun (feature, featData) -> feature, getMaxCatProb featData) 
+                                |> Seq.map (fun (feature, cat) -> feature, Category(cat)) |> Seq.toList
+                    Choice(feature, featureSelector, partData)
+            | _ -> let bestFeatureName, bestFeatureSel, others =  getOptFeature None restFeatures restData []
+                   let uniqueCategory = Seq.groupBy categorySelector >> Seq.length >> (=)1
+                   let catPart, choicePart = restData 
+                                                |> Seq.groupBy bestFeatureSel |> Seq.toList
+                                                |> List.partition (snd >> uniqueCategory)
+                   let catNodes = catPart 
+                                    |> Seq.map (fun (featVal, featData) -> featVal, Category(featData |> Seq.head |> categorySelector, 1.0))
                                     |> Seq.toList
-               Choice(bestFeature, catNodes@choiceNodes)
+                   let choiceNodes = choicePart 
+                                        |> Seq.map (fun (featVal, featData) -> featVal, trainID3Int featData others )
+                                        |> Seq.toList
+                   Choice(bestFeatureName, bestFeatureSel, catNodes@choiceNodes)
 
-    let rootNode = match features with
-                    | [] -> let cat = getMaxCatProb data
-                            Category(cat)
-                    | _ -> trainID3Int data features
-    Root(rootNode)
+        let rootNode = match features with
+                        | [] -> let cat = getMaxCatProb data
+                                Category(cat)
+                        | _ -> trainID3Int data features
+        Root(rootNode)
 
+    let displayTree (tree:Tree<'T, 'U>) =
+        let append (sb:System.Text.StringBuilder) (s:string) = sb.Append(s) |> ignore
+        let appendLine (sb:System.Text.StringBuilder) (s:string) = sb.Append("\n"+s) |> ignore
+        let tabs nb = String.replicate nb "\t"
+
+        let rec displayNode (node:TreeNode<'T,'U>) (acc:System.Text.StringBuilder) lev c =
+            match node with
+            | Category(cat, prob) -> append acc <| sprintf " > (%d, %A, %f)" lev cat prob
+            | Choice(feature,_,nodes) ->
+                    appendLine acc <| sprintf "%s[feature:%s]" (tabs lev) feature
+                    nodes 
+                        |> List.iter (fun (value, node) -> 
+                                                appendLine acc <| sprintf "%s(val:%A)" (tabs (lev+1)) value 
+                                                displayNode node acc (lev+1) (c+1))
+                
+    
+        let root = match tree with |Root(r) -> r
+        let sb = new System.Text.StringBuilder()
+        displayNode root sb 0 0
+        sb.ToString()
 
 
 
--- Src/NowCoding/DecisionTrees/Script.fsx
+++ Src/NowCoding/DecisionTrees/Script.fsx
@@ -2,6 +2,8 @@
 // for more guidance on F# programming.
 
 #load "Lib.fs"
+open NowCoding
+
 
 type Gender = |Male |Female
 type Row = {Gender:Gender; Phd:bool; IsEvil:bool}
@@ -42,13 +44,12 @@ let genderSelector = fun (r:Row) -> r.Gender :> obj
 let phdSelector = fun (r:Row) -> r.Phd :> obj
 let isEvilSelector = fun (r:Row) -> r.IsEvil
 
-let e = ``DecisionTrees﻿``.entropy isEvilSelector data 
-let g = ``DecisionTrees﻿``.gain data isEvilSelector genderSelector 
-let g' = ``DecisionTrees﻿``.gain data isEvilSelector phdSelector  
+let e = DecisionTrees.entropy isEvilSelector data 
+let g = DecisionTrees.gain data isEvilSelector genderSelector 
+let g' = DecisionTrees.gain data isEvilSelector phdSelector  
 
-let evTree = ``DecisionTrees﻿``.trainID3 data isEvilSelector [phdSelector; genderSelector]
+let evTree = DecisionTrees.trainID3 data isEvilSelector ["phd",phdSelector; "gender",genderSelector]
 
-Printf.k
 
 type Mov = {Action:bool; SciFi:bool; Actor:string}
 
@@ -56,12 +57,12 @@ let catSelector = fun (m:Mov) -> m.Actor
 let actionSelector = fun (m:Mov) -> m.Action :> obj
 let sciFiSelector = fun (m:Mov) -> m.SciFi :> obj
 
-let root = ``DecisionTrees﻿``.Choice(
-                sciFiSelector, [
-                        (true :> obj, ``DecisionTrees﻿``.Category("A", 1.0))
-                        (false:> obj, ``DecisionTrees﻿``.Choice(actionSelector, [
-                                                                  (true :> obj, ``DecisionTrees﻿``.Category("S", 1.0));
-                                                                  (false:> obj, ``DecisionTrees﻿``.Category("A", 1.0))
+let root = DecisionTrees.Choice(
+                "scifi", sciFiSelector, [
+                        (true :> obj, DecisionTrees.Category("A", 1.0))
+                        (false:> obj, DecisionTrees.Choice("action", actionSelector, [
+                                                                  (true :> obj, DecisionTrees.Category("S", 1.0));
+                                                                  (false:> obj, DecisionTrees.Category("A", 1.0))
                         ]
                         ))
                 ])
@@ -73,5 +74,47 @@ let d = [|
             {Action=true;SciFi=true;Actor="A"};
             {Action=true;SciFi=true;Actor="A"}
         |]
-let tree = ``DecisionTrees﻿``.trainID3 d catSelector [actionSelector; sciFiSelector]
+
+let tree = DecisionTrees.trainID3 d catSelector ["action",actionSelector; "scifi",sciFiSelector]
+
+// f1: [a1; a2; a3]
+// f2: [b1; b2; b3]
+// y : [y1; y2; y3]
+let data2 = [
+        ("a1", "b1", "y1");
+        ("a1", "b2", "y2");
+        ("a2", "b3", "y3");
+        ("a3", "b3", "y1");
+]
+
+let f1 = fun (x,_,_) -> x :>obj
+let f2 = fun (_,x,_) -> x :>obj
+let y  = fun (_,_,y) -> y
+
+let tree2 = DecisionTrees.trainID3 data2 y ["f1", f1;"f2", f2;]
+
+let vals = [|
+            data2 |> List.map (f1>>string) |> List.distinct;
+            data2 |> List.map (f2>>string) |> List.distinct;
+            |]
+//let allCombs = [|
+//                for x1 in vals.[0] do
+//                    for x2 in vals.[1] -> (x1,x2,"")|]
+
+let allCombs = [|
+                ("a1", "b1", ""); 
+                ("a1", "b2", ""); 
+                ("a1", "b3", ""); 
+                ("a2", "b1", "");
+                ("a2", "b2", ""); 
+                ("a2", "b3", ""); 
+                ("a3", "b1", ""); 
+                ("a3", "b2", "");
+                ("a3", "b3", "")|]
+let allClassified = allCombs |> Array.map (fun x -> DecisionTrees.classify x tree2)
+
+printf "%s" (DecisionTrees.displayTree tree2)
+               
+
+
 
